7ca0fcba2f89edf09701253826d6db6e79ed46f6
Rumesh851 <32399368+Rumesh851@users.noreply.github.com>
2018-11-19 16:00:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var uc = require('upper-case');
+var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(uc("Hello World!"));
     res.end();
-}).listen(8080);
+}).listen(port);
